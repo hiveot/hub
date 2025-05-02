32edbd664afdfd87b757389b34e5b4d10fc1d731
Henk <henk@spaaij.net>
2025-05-02 09:57:19 -0700

    Edit tile make source list sortable. Add a sortable list using drag & drop. Bug fixes.

--- services/hiveoview/hiveoview-README.md
+++ services/hiveoview/hiveoview-README.md
@@ -8,37 +8,69 @@ This viewer has reached alpha status with Phase 1-3 completed. The information b
 
 ### Phase 1: SSR infrastructure and session management [done]
 ### Phase 2: Directory view [done]
-### Phase 3: Basic Dashboard [done]
-
+### Phase 3: Basic Dashboard (v1) [done]
 ### Phase 4: Request/Response Handling Improvements [in progress]
-* Re-usable DataSchema component/template with text,number,bool,enum,on-off [in progress]
-* Track action progress using async response messages (ActionStatus) [in progress]
+* Show action progress using async response messages (ActionStatus) [in progress]
 * Indication of pending property writes (owserver updates can take 10 seconds)
+* redirect to login when SSE receives unauthorized error (after server restart)
+* Use cached values instead of reload when editing tiles
+* Only view authorized sources
 
-### Phase 5: Enhancements [todo]
-* multiple dashboards
-* configure 
+### Phase 5: Dashboard enhancements [in progress]
+* multiple dashboards [done]
+* Only show current dashboard tab on small screens
+* Manual sort of tile sources in edit tile [done]
+* dashboard configuration improvements [done]
   * dashboard static background image from file [done]
-  * dashboard background image from URL
-  * dashboard periodically refresh background image
-  * configure tile transparency, background and color [in progress]
+  * dashboard background image from URL [done]
+  * configure tile transparency, background and color [done]
   * configure allowing tile placement anywhere  [done]
 * only allow edit/move tiles if the user has permissions to edit, eg a manager+
   * depends on the authz change to define roles in the TD
 * briefly fade in/out a highlight of a changed value (css transition?)
-* color value based on age - red is older than 3 days
-* redirect to login when SSE receives unauthorized error (after server restart)
+* option to color value based on age 
 * show value error status in tiles
 * disable actions and edits for things that are not reachable (agent offline, no auth) 
 * Distinguish between basic and advanced attr/config/events using @type
    (TD does not support this facility, so use a config with @type values for basic values)
+* Improve use of Thing vocabulary types in bindings
+
+### Phase 6: Dashboard v2
+* Dashboard groups
+  * The dashboard consists of a grid containing tile groups
+  * Each tile group consists a grid of tiles
+  * Each tile contains a grid of widgets
+  * 
+* Customizable dashboard tiles
+  * Tile template using grid layout
+  * Select widget for one or more grid cell 
+  * Configure tile with yaml
+  * Widget placements in tile templates
+  * Properties, events and action in/output as widget sources
+* Built-in control widgets 
+  * on/off toggle switch (lights, media control, valves, ...)
+  * multi-value selector switch (for enums)
+  * ranged control dial knob (dimmer, volume, ...)
+  * ranged control slider (dimmer, volume, ...)
+* Built-in indicators widgets
+  * text gauge (values)
+  * alarm indicator (motion)
+  * dial gauge (ranged sensors such as humidity, temperature, )
+    * min/max alarm threshold indicator - alarm color
+  * horizontal/vertical slider gauge (ranged sensors such as humidity, temperature, )
+    * min/max alarm threshold indicator - alarm color
+  * history line/bar/scatter graph
+    * 24 hours
+    * current/min/max value
+  * image widget (URL)
+* Custom html widgets
 
 
-## Objective
+## Objectives
 
 Provide a reference implementation of a viewer on IoT data using the Hive Of Things backend. This includes a dashboard for presenting 'Thing' (device) outputs and controls for inputs.
 
-Use HTMX with bare minimum of javascript. Just HTML, HTMX, CSS, Golang and web components. No frameworks, no nodejs, no JS/TS/CSS compile step. Single binary for easy deployment. 
+Use HTMX with minimal of javascript. Just HTML, HTMX, CSS, Golang and web components. No frameworks, no nodejs, no JS/TS/CSS compile step. Single binary for easy deployment. 
 
 ## Summary
 
@@ -46,17 +78,21 @@ This service provides a web viewer for displaying IoT data.
 
 It primary views are login, directory and one or more live dashboards that dynamically update as values change. The dashboard consists of tiles that present single or multiple values. Multiple dashboard pages can be added.
 
-The frontend is generated from HTML/CSS templates and uses HTMX for interactivity. It presents a directory and a dashboard with tiles that can be configured to display 'thing' values.
+The frontend is generated from golang HTML/CSS templates and uses HTMX and JS for interactivity. It presents a directory and a dashboard with tiles that can be configured to display 'thing' values.
 
-## Implementation
+## Design
 
-This viewer is implemented using golang, chi, go html templates, htmx, picocss and a sprinkle of javascript where needed. No UI framework such as react, vue, or svelte is used in the creation of this viewer. Instead HTML is rendered server side using htmx and go templates, styled using picocss and served using chi router. Web components are used for providing client side behavior.
+This viewer is implemented using golang, chi, go html templates, htmx, picocss and a sprinkle of javascript where needed. No UI framework such as react, vue, or svelte is used in the creation of this viewer. HTML is rendered server side using htmx and go templates, styled using picocss and served using chi router. Web components are used for providing client side behavior.
 
 Dashboards:
 
-The dashboard consists of a collection of tiles arranged in a grid. Tiles can be added, removed, moved and resized.
+The dashboard consists of a collection of tiles arranged in a grid. Tiles can be added, removed, moved and resized to occupy one or more dashboard grid cells.
 
-Each tile has a presentation type that determines if it is text, a table, graph or gauge. Additional types can be added easily. Types are mapped to a html template that is inserted in the grid.
+Each tile consists of a configurable grid. Widgets are placed to occupy one or multiple tile grid cells. 
+
+Widgets provides text, a graph, gauge, image or other graphic element.  A widget has zero or more data sources. A data source can be a Thing property, event, action output. A widget can have zero or more actuator outputs. An actuator output can be a Thing property (configuration) or action input.
+
+has a presentation type that determines if it is text, a table, graph or gauge. Additional types can be added easily. Types are mapped to a html template that is inserted in the grid.
 
 The tile editor constructs a html element from the configuration and stores this for rendering.
 
---
+++ services/hiveoview/src/webcomp/h-list.js
@@ -0,0 +1,208 @@
+/**
+ * List component presenting a list of sortable items.
+ *
+ * This is a ul element which listens for drag&drop of li elements.
+ * Classes can be used to style the list using the ulClass attribute
+ * The default slot is injected into the light DOM so application styles apply.
+ *
+ * @attr striped alternate background each row  (uses css)
+ * @attr border include a border around the list
+ * @attr titlerow on li element to format in small caps
+ * @attr ulClass the class to assign to the internal ul list element
+ *
+ * Use draggable="true" for draggable row li elements.
+ * Use 'drag-handle' classname on the element used for dragging
+ * For example:
+ *  <h-list border striped ulClass="">
+ *      <li draggable="true" titlerow>
+ *          <div class=drag-handle>...</div>
+ *          <div>stuff</div>
+ *      </li>
+ *  </h-list>
+ */
+const UL_CLASS_ATTR = "ulClass"
+const template = `
+<ul class="h-list" >
+    <slot>list empty slot content</slot>
+</ul>
+
+<style>
+
+.dragging {
+    opacity: 0.7;
+    transform: rotate(-2deg);
+}
+
+.h-list  {
+padding-inline-start:0;
+}
+
+[border] .h-list {
+    border: 1px solid var(--pico-form-element-border-color);
+}
+
+.h-list li {
+    display: flex;
+    flex-direction: row;
+    gap: var(--pico-spacing);
+    list-style: none;
+}
+
+/* show the insert point when hovering*/
+/*FIXME: support for insert above*/
+.h-list li.hover {
+    /*background: green !important; !* testing dragging*!*/
+    /*Inserts below*/
+    border-bottom: 2px solid green !important;
+}
+
+/*.h-list li div {*/
+/*    font-weight: var(--pico-font-weight);*/
+/*    background-color: var(--pico-card-background-color);*/
+    /*padding: calc(var(--pico-spacing) / 2) var(--pico-spacing);*/
+/*}*/
+
+/* when list is striped*/
+[striped] .h-list li:nth-child(even)  {
+    background-color: var(--pico-form-element-active-background-color);
+}
+
+/* title of first row if set*/
+.h-list li[titlerow]   {
+    font-variant-caps: small-caps;
+    --pico-font-weight: 600;
+    border-bottom: 1px solid var(--pico-form-element-active-border-color);
+}
+
+</style>
+`
+// let baseClass = document.createElement('ul').constructor
+// const HList = class extends baseClass {
+class HList extends HTMLElement {
+
+    constructor() {
+        super();
+        this.draggedEl = undefined
+        this.dragEnterCounter = 0 // incr on enter, decr on leave
+        this.ulEl = undefined
+    }
+
+    // supports slots in light DOM
+    // https://frontendmasters.com/blog/light-dom-only/
+    childrenToSlots(html) {
+        let templateEl = document.createElement("template");
+        templateEl.innerHTML = html;
+
+        // move the slots children into the slots (eg, what shadow root does)
+        const slots = templateEl.content.querySelectorAll("slot");
+        for (const slot of slots) {
+            if (slot.name === "") {
+                // the default slot in the template is replaced with the children
+                const slotChildren = this.childNodes
+                slot.replaceWith(...slotChildren);
+                // templateEl.classList = slot.classList
+            } else { // named slot
+                const slotChildren = this.querySelectorAll(`[slot='${slot.name}']`);
+                slot.replaceWith(...slotChildren);
+            }
+        }
+        // finally assign the new template to this container element
+        this.replaceChildren(templateEl.content);
+        // this.replaceWith(templateEl.content)
+    }
+
+    connectedCallback() {
+        this.childrenToSlots(template)
+
+        // assign the ulClass attribute
+        const ulEl = this.querySelectorAll("ul");
+
+        const ulClassAttr = this.getAttribute(UL_CLASS_ATTR)
+        if (ulClassAttr) {
+            let tokens = ulClassAttr.split(" ")
+            ulEl[0].classList.add(...tokens)
+        }
+
+        // All li listen for drag events on all 'li' elements but only 'draggable' items can be dragged
+        // let rows = this.querySelectorAll('li[draggable="true"],li[titlerow]');
+        let rows = this.querySelectorAll('li');
+
+        for (let i = 0; i < rows.length; i++) {
+            const item = rows[i]
+
+            // only draggable items can be dragged (eg not the title row)
+            if (item.hasAttribute("draggable")) {
+                item.addEventListener('dragstart', (ev) => {
+                    console.log("dragstart")
+                    this.draggedEl = item
+                    ev.dataTransfer.dropEffect = "move"
+                    ev.dataTransfer.effectAllowed = "move"
+                    ev.dataTransfer.setData("text/html", item.id)
+                })
+            }
+            item.addEventListener('dragover', (ev) => {
+                // console.log("dragover")
+                // ??? why?
+                ev.preventDefault();
+            })
+            item.addEventListener('dragend', (ev) => {
+                console.log("dragend")
+                if (this.draggedEl) {
+                    this.draggedEl.classList.remove('dragging');
+                }
+                for (let r of items) {
+                    r.classList.remove("hover")
+                }
+            })
+            item.addEventListener('drop', (ev) => {
+                console.log("drop")
+                ev.stopPropagation()
+                ev.preventDefault();
+                // move the dragged element after this element
+                if (this.draggedEl !== item) {
+                    this.draggedEl.remove()
+                    item.after(this.draggedEl)
+                }
+            })
+            item.addEventListener('dragenter', (ev) => {
+                // if this is a different target, clear the current target
+                if (this.enterTarget && this.enterTarget !== ev.currentTarget ) {
+                    this.dragEnterCount = 0
+                    this.enterTarget.classList.remove("hover")
+                }
+
+                this.enterTarget = ev.currentTarget
+                this.dragEnterCounter++
+                console.log("dragenter; target", item.id)
+                // ev.stopPropagation()
+                ev.preventDefault()
+                item.classList.add("hover")
+            })
+            item.addEventListener('dragleave', (ev) => {
+                this.dragEnterCounter--
+                if (this.dragEnterCounter === 0) {
+                    console.log("dragleave; target", item.id)
+                    // ev.stopPropagation()
+                    // ev.preventDefault()
+                    item.classList.remove("hover")
+                    this.enterTarget = undefined
+                }
+            })
+        }
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === UL_CLASS_ATTR) {
+            // TODO
+        }
+    }
+
+// striped and border work through css
+    static get observedAttributes() {
+        return [UL_CLASS_ATTR];
+    }
+
+}
+
+customElements.define('h-list', HList);
+// customElements.define('h-list', HList, {extends: 'ul'});
